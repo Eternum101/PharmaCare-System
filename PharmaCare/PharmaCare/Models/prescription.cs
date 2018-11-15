9d0dd1a1b7f76aff4c5f2d34e0cb51796a2253a8
Jakob <Jakobpaul13@gmail.com>
2018-11-15 13:29:57 +1000

    *Added database data*

---
+++ PharmaCare/PharmaCare/Models/prescription.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmaCare.Models
+{
+    public class prescription
+    {
+        public string PrescriptionID { get; set; }
+        public string DrugID { get; set; }
+        public string PatientID { get; set; }
+        public string DoctorID { get; set; }
+        public string PrescriptionDate { get; set; }
+        public string PrescriptionStatus { get; set; }
+        public float DrugDose { get; set; }
+        public string FirstTime { get; set; }
+        public string LastTime { get; set; }
+        public int TimesPerDay { get; set; }
+        public string StatusOfDoes { get; set; }
+        public string AdditionalInformation { get; set; }
+    }
+}
